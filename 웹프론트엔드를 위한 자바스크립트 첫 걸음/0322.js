a6da01f7cf1bcbb4db42dcf534c2481dcdac9cfc
yewoonn <129031924+yewoonn@users.noreply.github.com>
2024-03-22 12:21:59 +0900

    03/22

--- 웹프론트엔드를 위한 자바스크립트 첫 걸음/0322.js
+++ 웹프론트엔드를 위한 자바스크립트 첫 걸음/0322.js
@@ -9,7 +9,51 @@
 //2) 비동기 : 여러 작업을 동시에 처리, 하나의 스레드에서 동시에 작업 처리
 // -> 논블로킹 방식
 
+//setTime() - 비동기 함수,
+//매개 변수로 전달 받은 시간 만큼 기다렸다가 콜백 함수 호출
+
+//setTimeout 출력 이전에 종료 출력
 setTimeout(()=>{
     console.log("3초만 기다리세요");
-},3000)
+},3000);
 
+console.log("종료");
+
+//setTimeout 출력 이후에 종료를 출력하고 싶다면
+const work = (callback) => {
+    setTimeout(()=>{
+        console.log("3초만 기다리세요");
+        callback();
+    }, 3000);
+}
+
+
+work(() => {
+    console.log("종료");
+});
+
+//work A,B,C 비동기 처리
+const workA = () =>{
+    setTimeout(()=>{
+        console.log("5초만 기다리세요");
+    }, 5000);
+}
+
+const workB = () =>{
+    setTimeout(()=>{
+        console.log("5초만 기다리세요");
+    }, 5000);
+}
+
+const workC = () =>{
+    setTimeout(()=>{
+        console.log("10초만 기다리세요");
+    }, 10000);
+}
+
+//비동기 처리 함수
+const workD = () =>{
+    setTimeout(()=>{
+        console.log("WorkD");
+    }, 10000);
+}
